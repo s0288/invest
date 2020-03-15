16ddd2eb79db8b13a16121b1ed1deada8ef448f8
s0288 <alex.gansmann@gmail.com>
2020-03-15 11:09:50 +0100

    finished first version

--- run_pm.py
+++ run_pm.py
@@ -9,27 +9,33 @@ import datetime
 import papermill as pm
 import os
 
-stocks = ['KORI.PA'] #, 'BKD', 'DVA', 'GEN', 'CSU', 'ADUS', 'ALK', '0293.HK', 'UAL', 'WIZZ.L', 'AF.PA', 'LHA.DE', 'ACR.SG', 'CZR', 'CNTY', '0308.HK', 'MAR', 'WYND', 'TUI1.DE', 'CCL', 'RCL', 'NCLH', 'DB', 'CBK.DE', 'GS', 'SEB-A.ST', 'NDA-SE.ST', 'AMZN', 'FB', 'GOOG', 'AAPL']
+stocks = ['KORI.PA', 'BKD', 'DVA', 'GEN', 'CSU', 'ADUS', 'ALK', '0293.HK', 'WIZZ.L', 'AF.PA', 'LHA.DE', 'CZR', '0308.HK', 'MAR', 'WYND', 'RCL', 'NCLH', 'DB', 'GS', 'SEB-A.ST', 'NDA-SE.ST', 'AMZN', 'FB', 'GOOG', 'AAPL', 'NFLX', 'MUV2.MI', 'CS.PA', 'ALV.DE', 'ING']
 # remove fullstops
-stocks = [stock.replace(".", "").lower() for stock in stocks]
+names = [stock.replace(".", "").lower() for stock in stocks]
+
+df = pd.read_pickle('stock_returns.pkl')
 
 # create jupyter notebook for stock
+for i, row in enumerate(stocks):
+    name = names[i]
+    stock = stocks[i]
 
-for stock in stocks:
-    try:
-        print(f"creating jupyter notebook for {stock}")
-        output_ipynb = str(stock) + '.ipynb'
-        os.system(f"jupyter trust {output_ipynb}")
-        pm.execute_notebook('corona_invest.ipynb', output_ipynb, parameters = dict(ticker=stock))
+    # only run for new stocks
+    if sum(stock == df.symbol) == 0:
+        try:
+            print(f"creating jupyter notebook for {stock}")
+            output_ipynb = "./stocks/" + str(name) + '.ipynb'
+            os.system(f"jupyter trust {output_ipynb}")
+            pm.execute_notebook('corona_invest.ipynb', output_ipynb, parameters = dict(ticker=stock))
 
-    except Exception as e:
-        print(f"error creating notebook for {stock}: {e}")
-    # # download html file without input code from jupyter notebook
-    # try:
-    #     print('converting jupyter notebook to html...')
-    #     ## add /home/s0288/.virtualenvs/my-virtualenv/bin/ to specify directory of virtual env for subprocess - reverts to default python 2.7 otherwise!
-    #     nbconverttohtml_nocode ="/home/s0288/.virtualenvs/my-virtualenv/bin/jupyter nbconvert --ExecutePreprocessor.kernel_name=python3.6 --template=nbextensions --to=html ./stocks/" + str(stock) + ".ipynb --no-input"
-    #     os.system(nbconverttohtml_nocode)
-    # except Exception as e:
-    #     print(f'error creating html file for {stock}')
+        except Exception as e:
+            print(f"error creating notebook for {stock}: {e}")
+        # download html file without input code from jupyter notebook
+        try:
+            print('converting jupyter notebook to html...')
+            ## add /home/s0288/.virtualenvs/my-virtualenv/bin/ to specify directory of virtual env for subprocess - reverts to default python 2.7 otherwise!
+            nbconverttohtml_nocode ="/Users/Alex/anaconda3/envs/notadiet/bin/jupyter nbconvert --to html ./stocks/" + str(name) + ".ipynb --no-input"
+            os.system(nbconverttohtml_nocode)
+        except Exception as e:
+            print(f'error creating html file for {stock}')
 
