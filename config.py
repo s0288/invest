f386af77c093c7248f59d35a4df21f3ad3a86224
s0288 <alex.gansmann@gmail.com>
2020-03-16 13:58:33 +0100

    added news cycle

---
+++ config.py
@@ -0,0 +1,1 @@
+API_NEWS = '771bb38a2a9d472da46aedd658fb1348'
--- run_pm.py
+++ run_pm.py
@@ -9,33 +9,36 @@ import datetime
 import papermill as pm
 import os
 
-stocks = ['KORI.PA', 'BKD', 'DVA', 'GEN', 'CSU', 'ADUS', 'ALK', '0293.HK', 'WIZZ.L', 'AF.PA', 'LHA.DE', 'CZR', '0308.HK', 'MAR', 'WYND', 'RCL', 'NCLH', 'DB', 'GS', 'SEB-A.ST', 'NDA-SE.ST', 'AMZN', 'FB', 'GOOG', 'AAPL', 'NFLX', 'MUV2.MI', 'CS.PA', 'ALV.DE', 'ING']
+stocks = ['^GDAXI', '^GSPC', 'KORI.PA', 'BKD', 'DVA', 'GEN', 'CSU', 'ADUS', 'ALK', '0293.HK', 'WIZZ.L', 'AF.PA', 'LHA.DE', 'CZR', '0308.HK', 'MAR', 'WYND', 'RCL', 'NCLH', 'DB', 'GS', 'SEB-A.ST', 'NDA-SE.ST', 'AMZN', 'FB', 'GOOG', 'AAPL', 'NFLX', 'MUV2.MI', 'CS.PA', 'ALV.DE', 'ING', 'ETSY']
 # remove fullstops
 names = [stock.replace(".", "").lower() for stock in stocks]
 
-df = pd.read_pickle('stock_returns.pkl')
+# remove stock_returns.pkl, if it exists already
+if os.path.isfile('stock_returns.pkl'):
+    os.remove("stock_returns.pkl")
+    print("File Removed!")
+
+
 
 # create jupyter notebook for stock
 for i, row in enumerate(stocks):
     name = names[i]
     stock = stocks[i]
 
-    # only run for new stocks
-    if sum(stock == df.symbol) == 0:
-        try:
-            print(f"creating jupyter notebook for {stock}")
-            output_ipynb = "./stocks/" + str(name) + '.ipynb'
-            os.system(f"jupyter trust {output_ipynb}")
-            pm.execute_notebook('corona_invest.ipynb', output_ipynb, parameters = dict(ticker=stock))
+    try:
+        print(f"creating jupyter notebook for {stock}")
+        output_ipynb = "./stocks/" + str(name) + '.ipynb'
+        os.system(f"jupyter trust {output_ipynb}")
+        pm.execute_notebook('corona_invest.ipynb', output_ipynb, parameters = dict(ticker=stock))
 
-        except Exception as e:
-            print(f"error creating notebook for {stock}: {e}")
-        # download html file without input code from jupyter notebook
-        try:
-            print('converting jupyter notebook to html...')
-            ## add /home/s0288/.virtualenvs/my-virtualenv/bin/ to specify directory of virtual env for subprocess - reverts to default python 2.7 otherwise!
-            nbconverttohtml_nocode ="/Users/Alex/anaconda3/envs/notadiet/bin/jupyter nbconvert --to html ./stocks/" + str(name) + ".ipynb --no-input"
-            os.system(nbconverttohtml_nocode)
-        except Exception as e:
-            print(f'error creating html file for {stock}')
+    except Exception as e:
+        print(f"error creating notebook for {stock}: {e}")
+    # download html file without input code from jupyter notebook
+    try:
+        print('converting jupyter notebook to html...')
+        ## add /home/s0288/.virtualenvs/my-virtualenv/bin/ to specify directory of virtual env for subprocess - reverts to default python 2.7 otherwise!
+        nbconverttohtml_nocode ="/Users/Alex/anaconda3/envs/notadiet/bin/jupyter nbconvert --to html ./stocks/" + str(name) + ".ipynb --no-input"
+        os.system(nbconverttohtml_nocode)
+    except Exception as e:
+        print(f'error creating html file for {stock}')
 
